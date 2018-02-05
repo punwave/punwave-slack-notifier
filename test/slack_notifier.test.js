513a15afc3bbf2d9c5988d499d0696f277b408d9
Chun-Kai Wang <chunkai1312@gmail.com>
2018-02-05 16:59:26 +0800

    test: notifier

--- test/slack_notifier.test.js
+++ test/slack_notifier.test.js
@@ -6,10 +6,14 @@ import SlackNotifier from '../src/slack_notifier'
 /* eslint-disable no-unused-expressions */
 
 describe('SlackNotifier', () => {
-  const spy = sinon.spy(Slack.prototype, 'webhook')
+  let webhookStub
 
   beforeEach(() => {
-    spy.reset()
+    webhookStub = sinon.stub(Slack.prototype, 'webhook')
+  })
+
+  afterEach(() => {
+    webhookStub.restore()
   })
 
   describe('#constructor', () => {
@@ -32,18 +36,33 @@ describe('SlackNotifier', () => {
   })
 
   describe('#send()', () => {
-    it('should send message to slack channel', () => {
+    it('should send message to slack channel with promise return', () => {
       const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
       notifier.send({ text: [{ title: 'test', code: 'ok' }] })
-      expect(spy.calledOnce).to.be.true
+      expect(webhookStub.calledOnce).to.be.true
+    })
+
+    it('should send message to slack channel with callback function', () => {
+      webhookStub.yields(null, {})
+      const callback = sinon.spy()
+      const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
+      notifier.send({ text: [{ title: 'test', code: 'ok' }] }, callback)
+      expect(webhookStub.calledOnce).to.be.true
+      expect(callback.calledOnce).to.be.true
     })
 
     it('should override default settings for customized messages', () => {
       const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
       const message = { title: 'title', text: 'test', attachments: [] }
       notifier.send(message)
-      expect(spy.calledOnce).to.be.true
-      expect(spy.args[0][0]).to.eql(message)
+      expect(webhookStub.calledOnce).to.be.true
+      expect(webhookStub.args[0][0]).to.eql(message)
+    })
+
+    it('should throw error if callback not a function', () => {
+      const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
+      const message = { title: 'title', text: 'test', attachments: [] }
+      expect(() => notifier.send(message, false)).to.throw(TypeError)
     })
   })
 
@@ -51,8 +70,8 @@ describe('SlackNotifier', () => {
     it('should send success type message to slack channel', () => {
       const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
       notifier.success({ text: [{ title: 'test', code: 'success' }] })
-      expect(spy.calledOnce).to.be.true
-      expect(spy.args[0][0].attachments[0]).to.have.property('color', 'good')
+      expect(webhookStub.calledOnce).to.be.true
+      expect(webhookStub.args[0][0].attachments[0]).to.have.property('color', 'good')
     })
   })
 
@@ -60,8 +79,8 @@ describe('SlackNotifier', () => {
     it('should send warning type message to slack channel', () => {
       const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
       notifier.warning({ text: [{ title: 'test', code: 'warning' }] })
-      expect(spy.calledOnce).to.be.true
-      expect(spy.args[0][0].attachments[0]).to.have.property('color', 'warning')
+      expect(webhookStub.calledOnce).to.be.true
+      expect(webhookStub.args[0][0].attachments[0]).to.have.property('color', 'warning')
     })
   })
 
@@ -69,8 +88,8 @@ describe('SlackNotifier', () => {
     it('should send danger type message to slack channel', () => {
       const notifier = new SlackNotifier('https://hooks.slack.com/services/TOKEN')
       notifier.danger({ text: [{ title: 'test', code: 'danger' }] })
-      expect(spy.calledOnce).to.be.true
-      expect(spy.args[0][0].attachments[0]).to.have.property('color', 'danger')
+      expect(webhookStub.calledOnce).to.be.true
+      expect(webhookStub.args[0][0].attachments[0]).to.have.property('color', 'danger')
     })
   })
 })
